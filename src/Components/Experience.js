9e51dad5c688cd0639e579fcd0699b28340f7a99
Sameer Kumar <sam210594@gmail.com>
2024-02-17 01:09:58 +0530

    made ui rectifications

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -70,7 +70,7 @@ export default function Experience() {
     ]
 
     return (
-        <div name='technologies' className="bg-gradient-to-b from-gray-800 to-black w-full min-h-screen flex items">
+        <div name='technologies' className="bg-gradient-to-b from-gray-800 to-black w-full min-h-screen flex items-center">
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
                 <div className='mt-24 md:mt-0'>
                     <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
